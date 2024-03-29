8b81daa0cc7708e771336a8c3a101ba2eb529a42
jeremy776 <jeremy.kusuma2018@gmail.com>
2024-03-29 13:42:56 +0700

    style(): new theme

--- src/routes/[id]/+page.ts
+++ src/routes/[id]/+page.ts
@@ -1,10 +1,11 @@
 /** @type {import('./$types').PageLoad} */
 export function load(a) {
-	// console.log(a)
+	console.log(a)
 	return {
 		status: 200,
 		data: {
-			id: a.params.id
+			id: a.params.id,
+			url: a.url.href,
 		},
 	};
 }
