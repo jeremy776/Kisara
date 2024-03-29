c7d6ae0d7fa0235e0d4f31d473373434d041bc32
jeremy776 <jeremy.kusuma2018@gmail.com>
2024-03-29 13:30:06 +0700

    feat(): privacy policy & about us

---
+++ src/routes/about-us/+page.svelte
@@ -0,0 +1,39 @@
+<div class="navbar bg-base-100">
+	<div class="flex-1">
+		<a class="btn btn-ghost text-xl" href="/">Kisara</a>
+	</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal px-1">
+			<li><a href="/">Beranda</a></li>
+		</ul>
+	</div>
+</div>
+
+<div class="sm:flex items-center max-w-screen-xl">
+	<div class="sm:w-1/2 p-10">
+		<div class="image object-center text-center">
+			<img alt="img" src="https://i.imgur.com/WbQnbas.png" />
+		</div>
+	</div>
+	<div class="sm:w-1/2 p-5">
+		<div class="text">
+			<span class="text-gray-500 border-b-2 border-indigo-600 uppercase">About us</span>
+			<h2 class="my-4 font-bold text-3xl sm:text-4xl">
+				About <span class="text-indigo-600">Kisara</span>
+			</h2>
+			<p class="text-gray-400">
+				Kisara adalah website yang memungkinkan teman dari pengguna untuk mengirim pesan secara
+				rahasia kepada pengguna.
+			</p>
+			<p class="text-gray-400 mt-4">
+				Kisara dibuat karena kami melihat salah satu cela keamaan di salah satu website yang sama
+				yaitu <a href="https://secreto.site/" class="text-blue-400 underline">Secreto</a>. dengan
+				adanya cela tersebut kami ingin membuat website yang lebih aman dan lebih baik.
+			</p>
+			<p class="text-gray-400 mt-4">
+				Kirasa dibuat oleh 3 orang (2 orang afk) dan 1 orang yang menjadi fullstack developer dalam
+				project ini.
+			</p>
+		</div>
+	</div>
+</div>
